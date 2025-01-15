dd224f45faab92b8268cd1ef886caf1279acf970
Reynald EUGENIE <reynald.eugenie@gmail.com>
2025-01-15 10:43:14 -0400

    Delete Soluces/SoluceBackForth.py

--- Soluces/SoluceBackForth.py
+++
@@ -1,9 +0,0 @@
-exec(open('TestRobot.py').read())
-
-mouvement=[]
-for j in range(9): #On répète 9 fois...
-    mouvement.append("droite") #...l'ajout de la direction "droite".
-for j in range(9): #On répète 9 fois...
-    mouvement.append("gauche") #...l'ajout de la direction "gauche" pour revenir au départ.
-
-go(mouvement)
